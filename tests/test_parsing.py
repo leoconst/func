e7f7461d1d4ac239482b9f4e19c712e370cb077c
leoconst <35425444+leoconst@users.noreply.github.com>
2023-12-28 01:40:51 +0000

    Use single parametrised test for successful parsing

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,34 +1,36 @@
+import pytest
+
 from func.parsing import *
 from func.tokenising import tokenise
 
 
-def test_empty():
-    tokens = []
-    actual = parse(tokens)
-    expected = ModuleSyntax([])
-    assert actual == expected
-
-def test_example():
-    source = "name = 'World'\ngreet = print 'Hello, \\(name)!'"
+@pytest.mark.parametrize('source, expected', [
+    (
+        '',
+        ModuleSyntax([])
+    ),
+    (
+        "name = 'World'\ngreet = print 'Hello, \\(name)!'",
+        ModuleSyntax([
+            BindingSyntax(
+                'name',
+                StringExpressionSyntax(['World'])
+            ),
+            BindingSyntax(
+                'greet',
+                CallExpressionSyntax(
+                    IdentifierExpressionSyntax('print'),
+                    StringExpressionSyntax([
+                        'Hello, ',
+                        IdentifierExpressionSyntax('name'),
+                        '!',
+                    ])
+                )
+            ),
+        ])
+    ),
+])
+def test_success(source, expected):
     tokens = tokenise(source)
     actual = parse(tokens)
-    expected = ModuleSyntax([
-        BindingSyntax(
-            'name',
-            StringExpressionSyntax([
-                'World',
-            ])
-        ),
-        BindingSyntax(
-            'greet',
-            CallExpressionSyntax(
-                IdentifierExpressionSyntax('print'),
-                StringExpressionSyntax([
-                    'Hello, ',
-                    IdentifierExpressionSyntax('name'),
-                    '!',
-                ])
-            )
-        ),
-    ])
     assert actual == expected
