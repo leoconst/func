ddc45bfb36617ca3a1ecae6093a33cdd09fb7a07
leoconst <35425444+leoconst@users.noreply.github.com>
2024-01-05 21:15:12 +0000

    Add another test for running from source

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,11 +1,25 @@
+import pytest
+
 import func
 
 
-def test_run(capsys):
-    source = '''\
+@pytest.mark.parametrize('source, expected_output', [
+    (
+        '''\
+main = print 0\
+''',
+        '0\n'
+    ),
+    (
+        '''\
 main = print (add1 x)
 x = add1 40
-add1 = add 1'''
+add1 = add 1\
+''',
+        '42\n',
+    ),
+])
+def test_run(capsys, source, expected_output):
     func.run(source)
     captured = capsys.readouterr()
-    assert captured.out == '42\n'
+    assert captured.out == expected_output
