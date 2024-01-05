d8fba98397e0e59516d5afe521fe01a7ca5a26c5
leoconst <35425444+leoconst@users.noreply.github.com>
2024-01-05 14:22:41 +0000

    Add additional 'no main binding' tests

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -4,7 +4,16 @@ from func.compiler import *
 from func.analysis import *
 
 
-def test_no_main_binding():
-    module = Module({})
+@pytest.mark.parametrize('module', [
+    Module({}),
+    Module({
+        'a': Integer(3),
+    }),
+    Module({
+        'a': Integer(3),
+        'moin': Integer(0),
+    }),
+])
+def test_no_main_binding(module):
     with pytest.raises(CompilationError, match='No main binding defined'):
         compile_(module)
