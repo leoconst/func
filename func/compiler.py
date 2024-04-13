0fbb8115458cfdbeb9d03f5d16a83effb4263931
leoconst <35425444+leoconst@users.noreply.github.com>
2024-04-13 12:20:30 +0100

    Simplify compiler by not treating arguments specially

--- func/compiler.py
+++ func/compiler.py
@@ -26,28 +26,6 @@ def _dereference_identifiers(expression, bindings):
 
 def _compile_expression(expression, bindings):
     match _dereference_identifiers(expression, bindings):
-        case Call():
-            return _compile_call(expression, bindings)
-        case _:
-            CompilationError(f'Unsupported expression type: {expression}')
-
-def _compile_call(call, bindings):
-    argument = _dereference_identifiers(call.argument, bindings)
-    yield from _compile_argument(argument, bindings)
-    callable_ = _dereference_identifiers(call.callable_, bindings)
-    yield from _compile_callable(callable_, bindings)
-
-def _compile_callable(expression, bindings):
-    match expression:
-        case list() as raw:
-            return raw
-        case Call():
-            return _compile_call(expression, bindings)
-        case _:
-            raise CompilationError(f'Unsupported callable type: {expression}')
-
-def _compile_argument(expression, bindings):
-    match expression:
         case Integer(value):
             yield Opcode.PUSH
             yield value
@@ -61,7 +39,22 @@ def _compile_argument(expression, bindings):
         case Call():
             yield from _compile_call(expression, bindings)
         case _:
-            raise CompilationError(f'Unsupported argument type: {expression}')
+            CompilationError(f'Unsupported expression type: {expression}')
+
+def _compile_call(call, bindings):
+    argument = _dereference_identifiers(call.argument, bindings)
+    yield from _compile_expression(argument, bindings)
+    callable_ = _dereference_identifiers(call.callable_, bindings)
+    yield from _compile_callable(callable_, bindings)
+
+def _compile_callable(expression, bindings):
+    match expression:
+        case list() as raw:
+            return raw
+        case Call():
+            return _compile_call(expression, bindings)
+        case _:
+            raise CompilationError(f'Unsupported callable type: {expression}')
 
 def _extract_string(parts):
     match parts:
