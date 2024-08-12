835eef6e133539fce8bc644f96050072ed485bf6
leoconst <35425444+leoconst@users.noreply.github.com>
2024-08-12 23:39:39 +0100

    Use tabs instead of spaces

--- tests/test_repl.py
+++ tests/test_repl.py
@@ -4,33 +4,33 @@ from io import StringIO
 
 
 def test_keyboard_interrupt_aborts(monkeypatch):
-	_set_input(monkeypatch, [])
-	repl()
+    _set_input(monkeypatch, [])
+    repl()
 
 def test_print_expression_prints(capsys, monkeypatch):
-	_set_input(monkeypatch, ["print 'Hello!'"])
-	repl()
-	captured = capsys.readouterr()
-	assert captured.out == 'Hello!\n'
+    _set_input(monkeypatch, ["print 'Hello!'"])
+    repl()
+    captured = capsys.readouterr()
+    assert captured.out == 'Hello!\n'
 
 def test_other_expression_prints_nothing(capsys, monkeypatch):
-	_set_input(monkeypatch, ["'Hello there'"])
-	repl()
-	captured = capsys.readouterr()
-	assert captured.out == ''
+    _set_input(monkeypatch, ["'Hello there'"])
+    repl()
+    captured = capsys.readouterr()
+    assert captured.out == ''
 
 def test_binding_added_to_namespace(capsys, monkeypatch):
-	_set_input(monkeypatch, ['num = 37', 'print (integer_to_string num)'])
-	repl()
-	captured = capsys.readouterr()
-	assert captured.out == '37\n'
+    _set_input(monkeypatch, ['num = 37', 'print (integer_to_string num)'])
+    repl()
+    captured = capsys.readouterr()
+    assert captured.out == '37\n'
 
 
 def _set_input(monkeypatch, inputs):
-	inputs_iter = iter(inputs)
-	def fake_input(_prompt):
-		try:
-			return next(inputs_iter)
-		except StopIteration:
-			raise KeyboardInterrupt()
-	monkeypatch.setattr('builtins.input', fake_input)
+    inputs_iter = iter(inputs)
+    def fake_input(_prompt):
+        try:
+            return next(inputs_iter)
+        except StopIteration:
+            raise KeyboardInterrupt()
+    monkeypatch.setattr('builtins.input', fake_input)
