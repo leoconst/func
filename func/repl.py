4abc1bcc75197bb3c255d89e7538bc3b5e9d89fd
leoconst <35425444+leoconst@users.noreply.github.com>
2024-08-19 00:30:38 +0100

    Refactor REPL implementation

--- func/repl.py
+++ func/repl.py
@@ -1,36 +1,43 @@
 from .runtime import execute
 from .compiler import compile_, BUILTINS
-from .analysis import analyse, Module as Module, analyse_expression
+from .analysis import analyse_expression, Expression, Module
 from .syntax import parse_binding, parse_expression, ParseError
 from .tokens import tokenise, TokeniseError
 
 
 def repl():
-    bindings = dict(BUILTINS)
-
-    def analyse_new_expression(expression):
-        return analyse_expression(expression, bindings)
-
-    while True:
+    bindings = BUILTINS.copy()
+    while (line := _get_next_line()) is not None:
         try:
-            line = input('>>> ')
-        except KeyboardInterrupt:
-            return
-
-        try:
-            tokens = list(tokenise(line))
-
-            try:
-                syntax_binding = parse_binding(iter(tokens))
-            except ParseError:
-                syntax_expression = parse_expression(iter(tokens))
-                analysed_expression = analyse_new_expression(syntax_expression)
-                module = Module({'main': analysed_expression, **bindings})
-                program = compile_(module)
-                execute(program)
-            else:
-                analysed_value = analyse_new_expression(syntax_binding.value)
-                bindings[syntax_binding.name] = analysed_value
-
+            match _process_line(line, bindings):
+                case (name, value):
+                    bindings[name] = value
+                case Expression() as expression:
+                    _execute(expression, bindings)
+                case other:
+                    raise ValueError(other)
         except Exception as exception:
             print(f'Error: {exception}')
+
+def _get_next_line():
+    try:
+        return input('>>> ')
+    except KeyboardInterrupt:
+        return None
+
+def _process_line(line, bindings):
+    tokens = list(tokenise(line))
+    try:
+        syntax_binding = parse_binding(iter(tokens))
+    except ParseError:
+        syntax_expression = parse_expression(iter(tokens))
+        return analyse_expression(syntax_expression, bindings)
+    else:
+        name = syntax_binding.name
+        value = analyse_expression(syntax_binding.value, bindings)
+        return (name, value)
+
+def _execute(main, bindings):
+    module = Module({'main': main, **bindings})
+    program = compile_(module)
+    execute(program)
