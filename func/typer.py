f048d58e87a7990f7c8278ac9d2a01f28fc5a369
leoconst <35425444+leoconst@users.noreply.github.com>
2024-08-25 17:24:47 +0100

    Simplify how expectation errors are raised

--- func/typer.py
+++ func/typer.py
@@ -1,18 +1,12 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-
 from . import types
 from .analysed import *
 
 
 def get_type(expression):
     expectations = _Expectations()
-    type = _get_type(expression, expectations)
-    unresolved_expectations = expectations.get_all()
-    if unresolved_expectations:
-        raise TypeError_(unresolved_expectations)
-    return type
+    return _get_type(expression, expectations)
 
 def _get_type(expression, expectations):
     match expression:
@@ -33,26 +27,19 @@ def _get_type(expression, expectations):
         case _:
             raise TypeError(f'Cannot get type of expression: {expression}')
 
-@dataclass
-class Expectation:
-    type: Type
-    description: str
-
 def _get_lambda_type(lambda_, expectations):
     return_type = _get_type(lambda_.body, expectations)
     name = lambda_.parameter
-    parameter_expectation = expectations.get(name)
-    if parameter_expectation is None:
+    parameter_type = expectations.get(name)
+    if parameter_type is None:
         parameter_type = types.Named(name)
-    else:
-        parameter_type = parameter_expectation.type
     return types.Callable(parameter_type, return_type)
 
 def _get_parameter_type(parameter, expectations):
-    expectation = expectations.get(parameter.name)
-    if expectation is None:
+    type = expectations.get(parameter.name)
+    if type is None:
         raise TypeError_(f'Undefined parameter: {parameter.name}')
-    return expectation.type
+    return type
 
 def _get_call_type(call, expectations):
     callable_type = _get_type(call.callable_, expectations)
@@ -70,7 +57,7 @@ def _get_call_type(call, expectations):
 
 def _get_if_else_type(if_else, expectations):
     expectations.expect(if_else.condition,
-        Expectation(types.INTEGER, 'if-else condition to be of type'))
+        'if-else condition to be of type', types.INTEGER)
     match if_else.true:
         case Parameter() as parameter:
             checked_branch = if_else.false
@@ -80,42 +67,26 @@ def _get_if_else_type(if_else, expectations):
             other_branch = if_else.false
     expected_type = _get_type(checked_branch, expectations)
     expectations.expect(other_branch,
-        Expectation(expected_type, 'if-else branch types to match'))
+        'if-else branch types to match', expected_type)
     return expected_type
 
 class _Expectations:
 
     def __init__(self):
-        self._unnamed = []
-        self._named = {}
+        self._parameters = {}
 
-    def expect(self, expression, expectation):
+    def expect(self, expression, description, expected):
         match expression:
-            case Parameter(name) if name not in self._named:
-                self._named[name] = expectation
+            case Parameter(name) if name not in self._parameters:
+                self._parameters[name] = expected
             case _:
                 actual = _get_type(expression, self)
-                expected = expectation.type
                 if actual != expected:
-                    self._unnamed.append((expectation, actual))
+                    raise TypeError_(
+                        f'Expected {description} {expected}, got {actual}')
 
     def get(self, name):
-        return self._named.pop(name, None)
-
-    def get_all(self):
-        return self._unnamed
+        return self._parameters.pop(name, None)
 
 class TypeError_(Exception):
-
-    def __init__(self, expectations):
-        if (isinstance(expectations, str)):
-            super().__init__(expectations)
-            return
-        expectation_count = len(expectations)
-        if expectation_count != 1:
-            raise ValueError(
-                f'Expected only 1 expectation, got {expectation_count}')
-        expectation, actual = expectations[0]
-        message = (f'Expected {expectation.description} {expectation.type},'
-                   f' got {actual}')
-        super().__init__(message)
+    pass
