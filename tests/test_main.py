444fce6884b5d2612ff761cc63d9220cb96e77d2
leoconst <35425444+leoconst@users.noreply.github.com>
2024-08-27 04:54:09 +0100

    Patch using thin custom pytest fixture which wraps unittest.mock

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,32 +1,28 @@
 import testing
-from unittest.mock import patch
 
 from pathlib import Path
 
 import func.__main__ as func_main
 
 
-def test_run_repl():
-    result = object()
-    with patch('func.__main__.repl') as repl:
-        with testing.raises(SystemExit):
-            func_main.main()
+def test_run_repl(mocker):
+    repl = mocker.patch('func.__main__.repl')
+    with testing.raises(SystemExit, message=''):
+        func_main.main()
     repl.assert_called_with()
 
-def test_run_with_file(monkeypatch):
+def test_run_with_file(mocker):
     path = '/a/path'
-    monkeypatch.setattr('sys.argv', ['', '--file', path])
-    with patch('func.__main__.run_file') as run_file:
-        with testing.raises(SystemExit):
-            func_main.main()
+    mocker.patch('sys.argv', ['', '--file', path])
+    run_file = mocker.patch('func.__main__.run_file')
+    with testing.raises(SystemExit, message=''):
+        func_main.main()
     run_file.assert_called_with(Path(path))
 
-def test_run_with_file_raises_exception(monkeypatch):
+def test_run_with_file_raises_exception(mocker):
     error_message = 'An error message'
-    monkeypatch.setattr('sys.argv', ['', '--file', 'PATH'])
-    with patch('func.__main__.run_file', side_effect=Exception(error_message)):
-        with testing.raises(SystemExit, message=f'Error: {error_message}'):
-            func_main.main()
-
-def raise_(exception):
-    raise exception
+    mocker.patch('sys.argv', ['', '--file', 'PATH'])
+    mocker.patch('func.__main__.run_file',
+        side_effect=Exception(error_message))
+    with testing.raises(SystemExit, message=f'Error: {error_message}'):
+        func_main.main()
--- tests/test_repl.py
+++ tests/test_repl.py
@@ -3,34 +3,34 @@ from func.repl import repl
 from io import StringIO
 
 
-def test_keyboard_interrupt_aborts(monkeypatch):
-    _set_input(monkeypatch, [])
+def test_keyboard_interrupt_aborts(mocker):
+    _set_input(mocker, [])
     repl()
 
-def test_print_expression_prints(capsys, monkeypatch):
-    _set_input(monkeypatch, ["print 'Hello!'"])
+def test_print_expression_prints(capsys, mocker):
+    _set_input(mocker, ["print 'Hello!'"])
     repl()
     captured = capsys.readouterr()
     assert captured.out == 'Hello!\n'
 
-def test_other_expression_prints_nothing(capsys, monkeypatch):
-    _set_input(monkeypatch, ["'Hello there'"])
+def test_other_expression_prints_nothing(capsys, mocker):
+    _set_input(mocker, ["'Hello there'"])
     repl()
     captured = capsys.readouterr()
     assert captured.out == ''
 
-def test_binding_added_to_namespace(capsys, monkeypatch):
-    _set_input(monkeypatch, ['num = 37', 'print (integer_to_string num)'])
+def test_binding_added_to_namespace(capsys, mocker):
+    _set_input(mocker, ['num = 37', 'print (integer_to_string num)'])
     repl()
     captured = capsys.readouterr()
     assert captured.out == '37\n'
 
 
-def _set_input(monkeypatch, inputs):
+def _set_input(mocker, inputs):
     inputs_iter = iter(inputs)
     def fake_input(_prompt):
         try:
             return next(inputs_iter)
         except StopIteration:
             raise KeyboardInterrupt()
-    monkeypatch.setattr('builtins.input', fake_input)
+    mocker.patch('builtins.input', fake_input)
